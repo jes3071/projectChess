21fb9ab3fa96b6d221cbcd3799e88479d0cc071f
Clolo <jes3071@gmail.com>
2021-09-23 15:22:12 +0900

    드래그 해결해야함

---
+++ Assets/Scripts/BattleField/Dragable.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dragable : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    public GameObject parentObject;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        //parentObject = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("onClick!!");
+    }
+        
+
+        public void OnPointerDown(PointerEventData eventData)
+    {
+        //parentObject.transform.SetAsLastSibling();
+        //Debug.Log("onClick!!");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+        //Debug.Log(eventData.delta);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+        //transform.position = eventData.position;
+        rectTransform.anchoredPosition += eventData.delta;
+
+        //rectTransform.BroadcastMessage("Drag", transform, SendMessageOptions.DontRequireReceiver);
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        //eventData.delta = (0, 0);
+        rectTransform.anchoredPosition = new Vector2(0, 0);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag != null)
+        {
+            return;
+        }
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,30 +6,61 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour {
     
     private GridLayoutGroup grid;
-    public Transform parent;
-    private GameObject ImagePrefab;
+    private GameObject SquareEmpty;
+    private GameObject SquareBlue;
+    private GameObject SquareRed;
+
+    private List<GameObject> SquareList = new List<GameObject>();
+
+    private List<int[]> BasicMap = new List<int[]>();
+    private int[] toHorn = new int[] {0,1,8 };
+
 
     private void Awake()
     {
+        
         grid = gameObject.GetComponent<GridLayoutGroup>();
     }
 
     public void SetDynamicGrid()
     {
-        grid.cellSize = new Vector2(75, 75);
+        //grid.cellSize = new Vector2(75, 75);
 
-        grid.spacing = new Vector2(10, 10);
-        grid.startCorner = GridLayoutGroup.Corner.UpperLeft;
-        grid.startAxis = GridLayoutGroup.Axis.Horizontal;
-        grid.constraint = GridLayoutGroup.Constraint.Flexible;
+        //grid.spacing = new Vector2(10, 10);
+        //grid.startCorner = GridLayoutGroup.Corner.UpperLeft;
+        //grid.startAxis = GridLayoutGroup.Axis.Horizontal;
+        //grid.constraint = GridLayoutGroup.Constraint.Flexible;
 
         for(int i = 0; i < 64; i++)
         {
-            GameObject imageObject = Instantiate(ImagePrefab);
+            GameObject imageObject = Instantiate(SquareEmpty) as GameObject;           
            
             imageObject.transform.parent = transform;
             imageObject.transform.localScale = Vector3.one;
             imageObject.transform.position = transform.position;
+            imageObject.name = "child" + (i+1);
+
+            SquareList.Add(imageObject);
+
+            //imageObject.
+        }
+
+        for(int j = 0; j < BasicMap.Count; j++)
+        {
+            if(j % 2 == 1)
+            {
+                for (int k = 0; k < BasicMap[j].Length; k++)
+                {
+                    SquareList[BasicMap[j][k]].GetComponent<Image>().sprite = Resources.Load<Sprite>("UI/BattleSquareBlue");
+                }
+            }
+            else
+            {
+                for (int k = 0; k < BasicMap[j].Length; k++)
+                {
+                    SquareList[BasicMap[j][k]].GetComponent<Image>().sprite = Resources.Load<Sprite>("UI/BattleSquareRed");
+                }
+            }
         }
         
         
@@ -41,11 +72,29 @@ public class BoardManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        ImagePrefab = Resources.Load<GameObject>("Prefab/ImagePrefab");
-        if(ImagePrefab == null)
+        SquareEmpty = Resources.Load<GameObject>("Prefab/SquareEmpty");
+        SquareBlue = Resources.Load<GameObject>("Prefab/SquareBlue");
+        SquareRed = Resources.Load<GameObject>("Prefab/SquareRed");
+
+        if (SquareEmpty == null || SquareBlue == null || SquareRed == null)
         {
-            Debug.Log("ImagePrefab == null");
+            Debug.Log("Square SomeThing == null");
         }
+
+        int[] tempValue = new int[3];
+        tempValue[0] = 0;
+        tempValue[1] = 1;
+        tempValue[2] = 8;
+
+        int[] tempValue1 = new int[3];
+        tempValue1[0] = 55;
+        tempValue1[1] = 62;
+        tempValue1[2] = 63;
+
+        //BasicMap.Add(tempValue);
+        BasicMap.Add(toHorn);
+        BasicMap.Add(tempValue1);
+
 
         SetDynamicGrid();
 	}
