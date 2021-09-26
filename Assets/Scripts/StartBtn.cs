dfb3a0a1a3cba643501e5fcee8e9169a3f28d573
Clolo <jes3071@gmail.com>
2021-09-26 13:06:23 +0900

    버튼 이벤트

---
+++ Assets/Scripts/StartBtn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBtn : MonoBehaviour {
+
+    public GameObject UIPreset;
+    public GameObject UIBattle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BattleStartButton()
+    {
+        UIPreset.SetActive(false);
+        UIBattle.SetActive(true);
+    }
+}
