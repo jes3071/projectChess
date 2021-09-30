1b406358f0aeba488fd7e31210e09357ca909191
Clolo <jes3071@gmail.com>
2021-09-30 21:11:12 +0900

    메타 파일들?

---
+++ Assets/Scripts/HpCounting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpCounting : MonoBehaviour {
+
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //StartCoroutine(Count(GameMgr._instance.playerMoney+100, GameMgr._instance.playerMoney));
+
+
+
+
+
+    //IEnumerator Count(float target, float current)
+
+    //{
+
+    //    float duration = 0.5f; // 카운팅에 걸리는 시간 설정. 
+
+    //    float offset = (target - current) / duration;
+
+
+
+    //    while (current < target)
+
+    //    {
+
+    //        current += offset * Time.deltaTime;
+
+    //        moneyLabel.text = ((int)current).ToString();
+
+    //        yield return null;
+
+    //    }
+
+
+
+    //    current = target;
+
+    //    moneyLabel.text = ((int)current).ToString();
+
+    //}
+}
